b3e5305374e0b51f6b22c7f700df756e36c3c336
Ben Record <brecord@cricut.com>
2025-06-06 17:08:45 -0600

    Neovim - Colorschemes

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  priority = 100,
+  { "catppuccin/nvim", event = "VeryLazy" },
+  { "EdenEast/nightfox.nvim", event = "VeryLazy" },
+  { "folke/tokyonight.nvim", event = "VeryLazy" },
+  { "navarasu/onedark.nvim", event = "VeryLazy" },
+  { "projekt0n/github-nvim-theme", event = "VeryLazy" },
+  { "rebelot/kanagawa.nvim", event = "VeryLazy" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
