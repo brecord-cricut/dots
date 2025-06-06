6734329c9eb0578c3c437dff326a75dd1cb42374
Ben Record <brecord@cricut.com>
2025-06-06 17:17:50 -0600

    Neovim - Extras

---
+++ .config/nvim/lua/config/extras.lua
@@ -0,0 +1,11 @@
+return {
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.test.core" },
+}
