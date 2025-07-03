e74166f68326309165e8d262d951946ef4cd9f70
Ben Record <brecord@cricut.com>
2025-07-03 08:56:32 -0600

    Neovim - Updating colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,12 +6,11 @@ return {
   { "navarasu/onedark.nvim", event = "VeryLazy" },
   { "projekt0n/github-nvim-theme", event = "VeryLazy" },
   { "rebelot/kanagawa.nvim", event = "VeryLazy" },
-  { "rose-pine/neovim", event = "VeryLazy" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "tokyonight-night",
     },
   },
 }
