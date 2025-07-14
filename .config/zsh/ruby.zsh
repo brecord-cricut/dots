1d63290ee439c2681bc0d70140008eb08d68a193
Ben Record <benjamin.s.record@gmail.com>
2025-07-14 17:46:19 -0600

    zsh - ruby

---
+++ .config/zsh/ruby.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+[[ ! -t 0 ]] && return
+
+if command -v rbenv >/dev/null; then
+  eval "$(rbenv init -)"
+  export GEM_HOME="$XDG_DATA_HOME/gem"
+  export PATH="$GEM_HOME/bin:$PATH"
+fi
