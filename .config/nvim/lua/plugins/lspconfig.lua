50226625f4b4072e333afb467ee721284973573a
Ben Record <benjamin.s.record@gmail.com>
2025-07-24 16:43:52 -0600

    Hyprland

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -4,6 +4,7 @@ return {
     inlay_hints = { enabled = false },
     servers = {
       bashls = { filetypes = { "sh", "zsh" } },
+      hyprls = vim.uv.os_uname().sysname == "Linux" and {},
     },
   },
   keys = {
