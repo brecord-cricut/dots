0a3e0bed6f971ab6ddcfca638853b5e0d802c484
Ben Record <brecord@cricut.com>
2025-08-19 08:56:39 -0600

    Neovim - Dynamic loading of custom plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local config_dir = vim.fn.stdpath("config") .. "/lua/custom/plugins"
+if vim.fn.isdirectory(config_dir) == 1 then
+  for _, file in ipairs(vim.fn.readdir(config_dir)) do
+    if file:sub(-4) == ".lua" then
+      local ok, err = pcall(require, "custom.plugins." .. file:sub(1, -5))
+      if not ok then
+        vim.notify("Error loading plugin: " .. file .. "\n" .. err, vim.log.levels.ERROR)
+      end
+    end
+  end
+end
