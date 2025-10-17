ccce891adf30f08e1bce305284cba05ec6801a91
Ben Record <brecord@cricut.com>
2025-10-17 07:40:33 -0600

    zsh - Modifying when `rbenv` is executed

--- .config/zsh/ruby.zsh
+++ .config/zsh/ruby.zsh
@@ -2,7 +2,7 @@
 [[ ! -t 0 ]] && return
 
 if command -v rbenv >/dev/null; then
-  eval "$(rbenv init -)"
   export GEM_HOME="$XDG_DATA_HOME/gem"
   export PATH="$GEM_HOME/bin:$PATH"
+  eval "$(rbenv init -)"
 fi
