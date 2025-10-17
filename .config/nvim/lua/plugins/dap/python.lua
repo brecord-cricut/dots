958c9c58378be35092033b8feb3401aeff810b9a
Ben Record <brecord@cricut.com>
2025-10-17 07:46:49 -0600

    Neovim - Fixing python bugs

--- .config/nvim/lua/plugins/dap/python.lua
+++ .config/nvim/lua/plugins/dap/python.lua
@@ -1,8 +1,7 @@
-return function(cb, config)
+return function(_, config)
   print(vim.inspect(config))
   local opts = {
     args = { "-m", "debugpy.adapter" },
   }
-  local final = vim.tbl_extend("force", require("mason-nvim-dap.mappings.adapters.python"), opts)
   return vim.tbl_extend("force", require("mason-nvim-dap.mappings.adapters.python"), opts)
 end
