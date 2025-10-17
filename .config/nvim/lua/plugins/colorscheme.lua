brecord-cricut/dotfiles

Neovim - Colorschemes

.config/nvim/lua/plugins/colorscheme.lua
@@ -1,0 +1,17 @@
+return {
+  priority = 100,
+  { "catppuccin/nvim", event = "VeryLazy" },
+  { "EdenEast/nightfox.nvim", event = "VeryLazy" },
+  { "folke/tokyonight.nvim", event = "VeryLazy" },
+  { "navarasu/onedark.nvim", event = "VeryLazy" },
+  { "projekt0n/github-nvim-theme", event = "VeryLazy" },
+  { "rebelot/kanagawa.nvim", event = "VeryLazy" },
+  { "rose-pine/neovim", event = "VeryLazy" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}

brecord-cricut/dots

.config/nvim/lua/plugins/colorscheme.lua
@@ -1,0 +1,16 @@
+return {
+  priority = 100,
+  { "catppuccin/nvim", event = "VeryLazy" },
+  { "EdenEast/nightfox.nvim", event = "VeryLazy" },
+  { "folke/tokyonight.nvim", event = "VeryLazy" },
+  { "navarasu/onedark.nvim", event = "VeryLazy" },
+  { "projekt0n/github-nvim-theme", event = "VeryLazy" },
+  { "rebelot/kanagawa.nvim", event = "VeryLazy" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}