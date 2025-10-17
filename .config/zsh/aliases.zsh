f2b355673f2ea1a9af0f8199971f7c82c51eca11
Ben Record <brecord@cricut.com>
2025-10-17 07:41:04 -0600

    zsh - Fixing alias bugs

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,25 +3,13 @@
 
 aliases() {
   cd $ZSH_CUSTOM
-  $EDITOR aliases.zsh
-  cd - >/dev/null
-  _refresh_zsh
-}
-
-zshc() {
-  cd $ZSH_CUSTOM
-  $EDITOR -c "lua require('persistence').load()"
-  if [ -f $ZSH_UPDATE_TRIGGER_PATH ]; then
-    echo "Sourcing $HOME/.zshrc..."
-    source ~/.zshrc
-    rm $ZSH_UPDATE_TRIGGER_PATH
-  fi
+  nvim aliases.zsh && source aliases.zsh
   cd - >/dev/null
 }
 
 repos() {
   local dir="$REPOS/$1"
-  [[ -d $dir ]] && cd $dir || echo "\"$dir\" is not a directory."
+  cd $dir 2&> /dev/null || echo "\"$dir\" is not a directory."
 }
 _reposCompletion() {
   compadd -- ${(f)"$(ls -1d $REPOS/*(/) 2>/dev/null | xargs -n1 basename)"}
