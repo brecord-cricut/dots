b2a78a2cfa169bb4f0a81e38392a56a3bc0de454
Ben Record <benjamin.s.record@gmail.com>
2025-11-02 10:37:19 -0700

    zsh: updating history dups, and bootstrapping

--- .config/zsh/rc/zsh.zsh
+++ .config/zsh/rc/zsh.zsh
@@ -3,6 +3,12 @@
 HISTFILE="$XDG_CACHE_HOME/zsh/history"
 HISTSIZE=10000
 SAVEHIST=1000
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
 
 zstyle :compinstall filename "$ZDOTDIR/.zshrc"
 
@@ -29,27 +35,29 @@ add_plugin https://github.com/zsh-users/zsh-autosuggestions.git
 add_plugin https://github.com/zsh-users/zsh-completions
 add_plugin https://github.com/zsh-users/zsh-history-substring-search
 
+unset -f add_plugin
+
 bindkey -v
 export KEYTIMEOUT=1
 bindkey '^R' history-incremental-search-backward
 
 autoload edit-command-line
 zle -N edit-command-line
-bindkey -M vicmd e edit-command-line
+bindkey -M vicmd E edit-command-line
 
 zshc() {
   cd "$ZDOTDIR"
-  nvim -c "lua require('persistence').load()"
-  # The file $ZSH_STALE_PATH is created via an autocmd in Nevoim when exiting after altering a file in $ZDOTDIR
-  if [[ -f "$ZSH_STALE_PATH" ]]; then
-    echo ":: Sourcing zsh config..."
-    rm "$ZSH_STALE_PATH"
-    [[ -r "$HOME/.zshenv" ]] && source "$HOME/.zshenv"
-    [[ -r "$ZDOTDIR/.zshenv" ]] && source "$ZDOTDIR/.zshenv"
-    [[ -r "$HOME/.zprofile" ]] && source "$HOME/.zprofile"
-    [[ -r "$ZDOTDIR/.zprofile" ]] && source "$ZDOTDIR/.zprofile"
-    [[ -r "$HOME/.zshrc" ]] && source "$HOME/.zshrc"
-    [[ -r "$ZDOTDIR/.zshrc" ]] && source "$ZDOTDIR/.zshrc"
-  fi
-  cd - >/dev/null
+  case "$1" in
+  "d") pwd ;;
+  *)
+    nvim
+    # The file $ZSH_STALE_PATH is created via an autocmd in Nevoim when exiting after altering a file in $ZDOTDIR
+    if [[ -f "$ZSH_STALE_PATH" ]]; then
+      echo ":: Sourcing zsh config..."
+      rm "$ZSH_STALE_PATH"
+      exec zsh
+    fi
+    cd - >/dev/null
+    ;;
+  esac
 }
