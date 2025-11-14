1560102adfdb24fcdcf20bdd74138c3d92257a47
Ben Record <benjamin.s.record@gmail.com>
2025-11-14 04:13:50 -0700

    Isolating p10k bootstrapping

--- .config/zsh/rc/p10k.zsh
+++ .config/zsh/rc/p10k.zsh
@@ -1,5 +1,9 @@
 [[ -f "$XDG_CACHE_HOME/user/p10k-disabled" ]] && return
 
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 if [[ ! -d "$REPOS/powerlevel10k" ]]; then
   printf "Download and install Powerlevel10k? [y/n]: "
   read answer
@@ -26,4 +30,10 @@ fi
 
 if [[ -r "$REPOS/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
   source "$REPOS/powerlevel10k/powerlevel10k.zsh-theme"
+fi
+
+if [[ -n $DISPLAY || -n $WAYLAND_DISPLAY || $OSTYPE == darwin* ]]; then
+  [[ ! -f $ZDOTDIR/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
+else
+  [[ ! -f $ZDOTDIR/.p10k.tty.zsh ]] || source $ZDOTDIR/.p10k.tty.zsh
 fi
