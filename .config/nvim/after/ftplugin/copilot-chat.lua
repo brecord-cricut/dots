d922c11a8852de8c7cc6d8142476d25c37cfc05f
Ben Record <benjamin.s.record@gmail.com>
2025-11-13 22:20:31 -0700

    nvim updates

--- .config/nvim/after/ftplugin/copilot-chat.lua
+++
@@ -1,4 +0,0 @@
--- For some reason, options don't take unless they're scheduled for the future:
-vim.schedule(function()
-  vim.opt_local.spell = true
-end)
--- .config/nvim/after/ftplugin/cpp.lua
+++
@@ -1,6 +0,0 @@
--- Highlighting for tokens like `public`, `private`, and `const`
-vim.api.nvim_set_hl(0, "@type.qualifier", { link = "@keyword" })
-vim.api.nvim_set_hl(0, "@storageclass", { link = "@keyword" })
-
--- clangd generates an enormous amount of logs:
-vim.lsp.set_log_level(vim.lsp.log_levels.OFF)
