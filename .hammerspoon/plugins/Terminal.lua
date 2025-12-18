61e11617619c4337f58a789435193d8e092d9447
Ben Record <brecord@cricut.com>
2025-12-18 12:52:31 -0700

    Moved hammerspoon getter files

--- .hammerspoon/plugins/Terminal.lua
+++ .hammerspoon/plugins/Terminal.lua
@@ -1,10 +1,11 @@
-hs.hotkey.bind({ "cmd" }, "return", function()
-	local path = os.getenv("HOME") .. "/.config/user/terminal"
+local fn = function()
+	local path = os.getenv("HOME") .. "/.local/state/terminal"
 	if not hs.fs.attributes(path) then
 		print("Terminal config file not found: " .. path)
 		return
 	end
 	local file = io.open(path)
+
 	local terminal = file:read("*l")
 	file:close()
 	if terminal and #terminal > 0 then
@@ -12,4 +13,6 @@ hs.hotkey.bind({ "cmd" }, "return", function()
 	else
 		print("Terminal executable not found")
 	end
-end)
+end
+hs.hotkey.bind({ "cmd" }, "return", fn)
+hs.hotkey.bind({ "cmd" }, "t", fn)
