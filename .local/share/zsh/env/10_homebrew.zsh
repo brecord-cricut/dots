c9633a5df15ab1b237953f76faa3bd3dbf678544
Ben Record <brecord@cricut.com>
2026-02-18 18:57:54 -0700

    Updating Homebrew init

--- .local/share/zsh/env/10_homebrew.zsh
+++ .local/share/zsh/env/10_homebrew.zsh
@@ -1,5 +1,12 @@
-if [[ $OSTYPE == darwin* && ${+HOMEBREW_PREFIX} == 0 ]]; then
-  (( ${+commands[brew]} )) && eval "$(brew shellenv)"
+if [[ $OSTYPE == darwin* ]]; then
+  if (( ${+HOMEBREW_PREFIX} == 0 )); then
+    if [[ -x /opt/homebrew/bin/brew ]]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [[ -x /usr/local/bin/brew ]]; then
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
+  fi
+
+  # Run path_helper to ensure proper PATH ordering
   [[ -x /usr/libexec/path_helper ]] && eval "$(/usr/libexec/path_helper -s)"
-  path=(/opt/homebrew/bin(N-/) $path)
 fi
