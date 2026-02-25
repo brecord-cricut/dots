f346c1db32ec34b57b4a2c9950c35fd3a4323377
Ben Record <brecord@cricut.com>
2026-02-25 08:47:19 -0700

    Nextcloud env updates

--- .config/zsh/rc.d/nextcloud.zsh
+++ .config/zsh/rc.d/nextcloud.zsh
@@ -1,12 +1,14 @@
-export NEXTCLOUD_DIR="$XDG_DATA_HOME/Nextcloud"
+NEXTCLOUD_DIR="$XDG_DATA_HOME/Nextcloud"
 
-[[ -d "$NEXTCLOUD_DIR/bin" ]] && path=("$NEXTCLOUD_DIR/bin" $path[@])
+[[ -d "$NEXTCLOUD_DIR/bin" ]] && path=("$NEXTCLOUD_DIR/bin" $path[@]) || return 0
+
+export NEXTCLOUD_DIR
 
 notes() {
   local notes_path="$NEXTCLOUD_DIR/Notes"
   if [ -d "$notes_path" ]; then
     cd "$notes_path"
-    $EDITOR -c "lua require('persistence').load()" .
+    nvim
     cd - >/dev/null
   else
     echo "Notes directory not found: $notes_path"
