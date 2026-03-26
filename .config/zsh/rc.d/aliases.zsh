1584e886d3d4f7408e2f222e3a9027e13b6567e6
Ben Record <brecord@cricut.com>
2026-03-26 09:41:36 -0600

    zsh - Lazygit opening in a dotfiles context, when appropriate

--- .config/zsh/rc.d/aliases.zsh
+++ .config/zsh/rc.d/aliases.zsh
@@ -1,6 +1,14 @@
-DOTS_PREFIX=(GIT_DIR=$REPOS/dots GIT_WORK_TREE=$HOME)
+lazygit() {
+  if git rev-parse --git-dir &>/dev/null 2>&1; then
+    command lazygit "$@"
+  elif [[ -n "$(env "${DOTS_PREFIX[@]}" command git ls-files "$PWD" 2>/dev/null)" ]]; then
+    env "${DOTS_PREFIX[@]}" command lazygit "$@"
+  else
+    command lazygit "$@"
+  fi
+}
 
-dflg() { env "${DOTS_PREFIX[@]}" lazygit "$@"; }
+dflg() { env "${DOTS_PREFIX[@]}" command lazygit "$@"; }
 dots() { env "${DOTS_PREFIX[@]}" git "$@"; }
 
 alias ld="lazydocker"
