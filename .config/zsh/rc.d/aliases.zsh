829b5c780ce89ed7ffde271392e802162a1c316f
Ben Record <benjamin.s.record@gmail.com>
2026-04-17 10:31:27 -0600

    zsh - `opencode` alias

--- .config/zsh/rc.d/aliases.zsh
+++ .config/zsh/rc.d/aliases.zsh
@@ -15,5 +15,6 @@ dots() { env "${DOTS_PREFIX[@]}" git "$@"; }
 
 alias ld="lazydocker"
 alias lg="lazygit"
+alias oc="opencode"
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
