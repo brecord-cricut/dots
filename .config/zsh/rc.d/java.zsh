cc65e2824997a78b6b3edacf29d8631cdf708c56
Ben Record <brecord@cricut.com>
2026-04-20 15:01:12 -0600

    zsh - Updating Java

--- .config/zsh/rc.d/java.zsh
+++ .config/zsh/rc.d/java.zsh
@@ -1,8 +1,7 @@
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  export JAVA_HOME="$HOMEBREW_CELLAR/openjdk@17/17.0.17"
+  export JAVA_HOME="/opt/homebrew/opt/openjdk"
 elif [[ "$(uname -s)" == "Linux" ]]; then
   export JAVA_HOME="$XDG_DATA_HOME/openjdk/current"
 fi
 
 path=($JAVA_HOME/bin $path[@])
-
