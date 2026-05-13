d0a7a861a68e8ec7f1ee98521939276786f8c03b
Ben Record <brecord@cricut.com>
2026-05-13 10:45:59 -0600

    Neovim - Keymap to add to the quickfix list

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>xa", function()
+  local entry = {
+    bufnr = vim.api.nvim_get_current_buf(),
+    lnum = vim.fn.line("."),
+    col = vim.fn.col("."),
+    text = vim.fn.getline("."),
+  }
+  vim.fn.setqflist({ entry }, "a")
+  vim.cmd("copen")
+  vim.cmd("wincmd p")
+end, { desc = "Add location to quickfix" })
