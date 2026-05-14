b1a8f3cbf30957e1b70df6049d70f2310fec5fdb
Ben Record <brecord@cricut.com>
2026-05-14 16:05:00 -0600

    Neovim - `<C-g>` copies the relative file path to the system clipboard

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<C-g>", function()
+  local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+  vim.fn.setreg("+", path)
+  vim.notify(path)
+end, { desc = "Copy relative file path to clipboard" })
+
 vim.keymap.set("n", "<leader>xa", function()
   local entry = {
     bufnr = vim.api.nvim_get_current_buf(),
