19a475bedfa391de1bf5758e10157f63a4981a20
Ben Record <brecord@cricut.com>
2026-06-03 14:19:48 -0600

    Add notes environment variable

--- .config/zsh/rc.d/nextcloud.zsh
+++ .config/zsh/rc.d/nextcloud.zsh
@@ -9,7 +9,7 @@ notes() {
   local notes_path="$NEXTCLOUD_DIR/Notes"
   if [ -d "$notes_path" ]; then
     cd "$notes_path"
-    GIT_DIR= $EDITOR
+    GIT_DIR= NOTES=1 $EDITOR
     cd - >/dev/null
   else
     echo "Notes directory not found: $notes_path"
