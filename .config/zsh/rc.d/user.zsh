9f28b10b0e28294fd80de6cefc1540b1b60df41e
Ben Record <brecord@cricut.com>
2026-09-16 10:42:44 -0600

    Repos partial name matching

--- .config/zsh/rc.d/user.zsh
+++ .config/zsh/rc.d/user.zsh
@@ -1,9 +1,20 @@
 repos() {
   local dir="$REPOS/$1"
-  [[ -d "$dir" ]] && cd "$dir" || echo "\"$dir\" is not a directory."
+  if [[ -d "$dir" ]]; then
+    cd "$dir"
+    return
+  fi
+
+  # Match partial input: `repos f` would resolve to `cd $REPOS/foo`, if that directory exists.
+  local matches=("$REPOS"/$1*(N/))
+  if ((${#matches[@]} > 0)); then
+    cd "${matches[1]}"
+  else
+    echo "\"$dir\" is not a directory."
+  fi
 }
 
-if command -v compdef &> /dev/null; then
+if command -v compdef &>/dev/null; then
   _reposCompletion() {
     _files -W "$REPOS" -/
   }
